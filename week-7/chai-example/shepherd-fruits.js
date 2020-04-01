cfd548c6deb8f65bb803696541e36146288ea5a0
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-04-01 08:41:28 -0500

    added assignment 7.3

---
+++ week-7/chai-example/shepherd-fruits.js
@@ -0,0 +1,16 @@
+/*
+===============================================================================
+; Title:  shepherd-fruits.js
+; Author: Professor Krasso
+; Date:   4/1/2020
+; Modified By: Jeff Shepherd
+; Description: fruits function
+;==============================================================================
+*/
+"use strict";
+
+function fruits (aString) {
+  return aString.split(",");
+}
+
+module.exports = fruits;
